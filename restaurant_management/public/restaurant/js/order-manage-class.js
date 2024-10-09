12ec7487808cb736942cd07fcb1f02555f062c6d
helkyd <hcesar@gmail.com>
2024-10-09 19:32:05 +0000

    FIX

--- restaurant_management/public/restaurant/js/order-manage-class.js
+++ restaurant_management/public/restaurant/js/order-manage-class.js
@@ -937,6 +937,7 @@ class OrderManage extends ObjectManage {
                             cpj.clientPrinter = new JSPM.InstalledPrinter("POS80"); // new JSPM.InstalledPrinter($('#installedPrinterName').val());
                         }
                         //TESTE USING ESC/POS
+                        /*
                         // Create ESC/POS commands
                         var escposCommands = "\x1B\x40"; // Initialize printer
                         escposCommands += "\x1B\x21\x08"; // Emphasized mode
@@ -950,6 +951,44 @@ class OrderManage extends ObjectManage {
 
                         // Send print job to printer
                         cpj.sendToClient();
+                        */
+
+                        //Set content to print...
+                        //Create ESP/POS commands for sample label
+                        var esc = '\x1B'; //ESC byte in hex notation
+                        var newLine = '\x0A'; //LF byte in hex notation
+                    
+                        var cmds = esc + "@"; //Initializes the printer (ESC @)
+                        cmds += esc + '!' + '\x38'; //Emphasized + Double-height + Double-width mode selected (ESC ! (8 + 16 + 32)) 56 dec => 38 hex
+                        cmds += 'BEST DEAL STORES'; //text to print
+                        cmds += newLine + newLine;
+                        cmds += esc + '!' + '\x00'; //Character font A selected (ESC ! 0)
+                        cmds += 'COOKIES                   5.00'; 
+                        cmds += newLine;
+                        cmds += 'MILK 65 Fl oz             3.78';
+                        cmds += newLine + newLine;
+                        cmds += 'SUBTOTAL                  8.78';
+                        cmds += newLine;
+                        cmds += 'TAX 5%                    0.44';
+                        cmds += newLine;
+                        cmds += 'TOTAL                     9.22';
+                        cmds += newLine;
+                        cmds += 'CASH TEND                10.00';
+                        cmds += newLine;
+                        cmds += 'CASH DUE                  0.78';
+                        cmds += newLine + newLine;
+                        cmds += esc + '!' + '\x18'; //Emphasized + Double-height mode selected (ESC ! (16 + 8)) 24 dec => 18 hex
+                        cmds += '# ITEMS SOLD 2';
+                        cmds += esc + '!' + '\x00'; //Character font A selected (ESC ! 0)
+                        cmds += newLine + newLine;
+                        cmds += '11/03/13  19:53:17';
+                        cmds += newLine + newLine;
+                        cmds += newLine + newLine;                        
+                        cmds += "\x1D\x56\x41"; // Cut paper
+
+                        cpj.printerCommands = cmds;
+                        //Send print job to printer!
+                        cpj.sendToClient();                        
                         
                         /*
                         //Set content to print... 
