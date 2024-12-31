d1e61654f3170ca398f742309522189dc2aae261
helkyd <hcesar@gmail.com>
2024-12-31 10:14:55 +0000

    FIX

--- restaurant_management/restaurant_management/doctype/table_order/table_order.py
+++ restaurant_management/restaurant_management/doctype/table_order/table_order.py
@@ -762,6 +762,9 @@ class TableOrder(Document):
 
     def set_printed_status(self, identifier, itemcode):
         #FIX 19-10-2024
+        print ('identi ', identifier)
+        print ('itemmmm ', itemcode)
+
         if identifier:
             print ('SET PRINTED STATUZ DONE....')
             frappe.db.sql(""" UPDATE `tabOrder Entry Item` set was_printed = 1 where parent = %s and item_code = %s """, (identifier,itemcode), as_dict=False)
