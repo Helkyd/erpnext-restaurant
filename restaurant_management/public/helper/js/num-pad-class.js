9d6de9580b1dfb99b8f55911d6bf05354a0d046e
helkyd <hcesar@gmail.com>
2025-06-03 22:54:12 +0000

    FIx

--- restaurant_management/public/helper/js/num-pad-class.js
+++ restaurant_management/public/helper/js/num-pad-class.js
@@ -1,6 +1,7 @@
 class NumPad {
     #input = null;
     #html = "";
+    #grams = false;
     constructor(options) {
         Object.assign(this, options);
         this.make();
@@ -9,11 +10,18 @@ class NumPad {
     set html(val){this.#html = val}
     set input(val){this.#input = val}
 
+    //FIX 03-06-2025
+    set grams(val){this.#grams = val}
+
     get input(){return this.#input}
     get html(){return this.#html}
 
+    //FIX 03-06-2025
+    get grams(){return this.#grams}
+
     make() {
         console.log('NUM PAD CLASS - MAKE')
+
         const default_class = `pad-col button btn-default`;
 
         let num_pads = [
@@ -41,6 +49,39 @@ class NumPad {
                 1: {props: {class: "sm pad-btn"}},
                 2: {props: {class: "sm pad-btn"}},
                 3: {props: {class: "sm pad-btn"}},
+            },
+            {
+                0: {props: {class: "sm pad-btn", colspan: 2}},
+                '.': {props: {class: "sm pad-btn"}, action: "key"},
+            }
+        ];
+
+        //FIX 03-06-2025
+        let grams_num_pads = [
+            {
+                400: {props: {class: "sm pad-btn"}},
+                450: {props: {class: "sm pad-btn"}},
+                500: {props: {class: "sm pad-btn"}},
+                Del: {
+                    props: {class: "md pad-btn"},
+                    content: '<span class="fa fa-arrow-left pull-left" style="font-size: 16px; padding-top: 3px"></span>',
+                    action: "delete"
+                },
+            },
+            {
+                250: {props: {class: "sm pad-btn"}},
+                300: {props: {class: "sm pad-btn"}},
+                350: {props: {class: "sm pad-btn"}},
+                Enter: {
+                    props: {class: "md pad-btn", rowspan: "3"},
+                    content: '<br><br><span class="fa fa-level-down" style="font-size: 25px; transform: rotate(90deg);"></span>',
+                    action: "enter"
+                },
+            },
+            {
+                100: {props: {class: "sm pad-btn"}},
+                150: {props: {class: "sm pad-btn"}},
+                200: {props: {class: "sm pad-btn"}},
             },
             {
                 0: {props: {class: "sm pad-btn", colspan: 2}},
